42c81b77fc5a5d3a5ae536ce1d891150ffe09018
Tahereh <tahereh.rastegarzare@gmail.com>
2022-04-27 11:41:22 -0700

    SignalRDemo

---
+++ SignalRChat/SignalRChat/Hubs/ChatHub.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+//The ChatHub class inherits from the SignalR Hub class.
+//The Hub class manages connections, groups, and messaging.
+
+
+namespace SignalRChat.Hubs
+{
+    public class ChatHub : Hub
+    {
+        public async Task SendMessage(string user, string message)
+        {
+            await Clients.All.SendAsync("ReceiveMessage", user, message);
+        }
+    }
+}
+
